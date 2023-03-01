357bfaa223f23989f381f22e9a4aa96bdd15ad87
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2023-03-01 09:55:42 +0100

    menu

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const select = document.getElementById('select-all');
+
+console.log(select);
+select.addEventListener("click", function () {
+  var checkboxes = document.getElementsByClassName('page');
+  for (var checkbox of checkboxes) {
+    checkbox.checked = this.checked;
+  }
+});
+
+jQuery(document).ready(function () {
+
+  var updateOutput = function () {
+    $('#nestable-output').val(JSON.stringify($('#nestable').nestable('serialize')));
+  };
+
+  $('#nestable').nestable().on('change', updateOutput);
+
+  updateOutput();
+
+  $("#add-item").submit(function (e) {
+    e.preventDefault();
+    id = Date.now();
+    var label = $("#add-item > [name='name']").val();
+    if ((label == "")) return;
+    var item =
+      '<li class="dd-item dd3-item" data-id="' + id + '" data-label="' + label + '">' +
+      '<div class="dd-handle dd3-handle">Drag</div>' +
+      '<div class="dd3-content">' +
+      '<span>' + label + '</span>' +
+      '</div>' +
+      '</li>';
+
+    $("#nestable > .dd-list").append(item);
+    $("#nestable").find('.dd-empty').remove();
+    $("#add-item > [name='name']").val('');
+    $("#add-item > [name='url']").val('');
+    updateOutput();
+  });
+
+  $("body").delegate(".item-delete", "click", function (e) {
+    $(this).closest(".dd-item").remove();
+    updateOutput();
+  });
+
+
+  $("body").delegate(".item-edit, .item-close", "click", function (e) {
+    var item_setting = $(this).closest(".dd-item").find(".item-settings");
+    if (item_setting.hasClass("d-none")) {
+      item_setting.removeClass("d-none");
+    } else {
+      item_setting.addClass("d-none");
+    }
+  });
+
+  $("body").delegate("input[name='navigation_label']", "change paste keyup", function (e) {
+    $(this).closest(".dd-item").data("label", $(this).val());
+    $(this).closest(".dd-item").find(".dd3-content span").text($(this).val());
+  });
+});
--- menuHelper.php
+++ menuHelper.php
@@ -11,7 +11,8 @@ Author URI: http://mon-siteweb.com/
 function my_admin_menu()
 {
   add_theme_page('Menu Helper', 'Menu Helper', 'edit_pages', 'menuHelper.php', 'myplguin_admin_page', 3);
-  wp_enqueue_style('styles_css', plugins_url('styles.css', __FILE__), array());
+  wp_enqueue_style('styles_css', plugins_url('css/style.css', __FILE__), array());
+  wp_enqueue_style('stylesJqu_css', plugins_url('css/jquery.nestable.css', __FILE__), array());
 }
 
 function myplguin_admin_page()
@@ -32,12 +33,17 @@ function myplguin_admin_page()
 
       <button>Ajouter au menu</button>
     </div>
-    <div class="container_menu">
-      bonjour
+    <div class="dd" id="nestable">
+      <?php
+      all_pages2();
+      ?>
     </div>
   </div>
 <?php
-  wp_enqueue_script('script_js', plugins_url('script.js', __FILE__), array());
+  wp_enqueue_script('script_js', plugins_url('js/script.js', __FILE__), array());
+  wp_enqueue_script('script_js', plugins_url('js/script2.js', __FILE__), array());
+  wp_enqueue_script('scriptjq_js', plugins_url('js/jquery-3.4.1.min.js', __FILE__), array("jquery"));
+  wp_enqueue_script('scriptjq2_js', plugins_url('js/jquery.nestable.js', __FILE__), array("jquery"));
 }
 
 function all_pages()
@@ -53,9 +59,48 @@ function all_pages()
   }
 }
 
-
-
-// Now we set that function up to execute when the admin_notices action is called.
-if (is_admin()) { // admin actions
-  add_action('admin_menu', 'my_admin_menu');
+function all_pages2()
+{
+  $pages = get_pages();
+?>
+  <ol class="dd-list">
+    <?php
+    foreach ($pages as $page) {
+      menuItem($page->ID, $page->post_title);
+    ?>
+      </li>
+    <?php
+    }
+    ?>
+  </ol>
+<?php
 }
+
+function menuItem($id, $label)
+{
+?>
+  <li class="dd-item dd3-item" data-id="<?= $id ?>" data-label="<?= $label ?>">
+    <div class="dd-handle dd3-handle">
+      Drag
+    </div>
+    <div class="dd3-content">
+      <span><?= $label ?></span>
+      <div class="item-edit">Edit</div>
+    </div>
+    <div class="item-settings d-none">
+      <p>
+        <label for="">Titre de la navigation<br>
+          <input type="text" name="navigation_label" value="<?= $label ?>">
+        </label>
+      </p>
+      <p>
+        <a class="item-delete" href="javascript:;">Retirer</a>
+        |
+        <a class="item-close" href="javascript:;">Annuler</a>
+      </p>
+    </div>
+  <?php
+}
+
+
+add_action('admin_menu', 'my_admin_menu');
