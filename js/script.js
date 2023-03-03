d6185a74a581c412c39335c3450121ec16fb659b
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2023-03-03 10:45:03 +0100

    insert in db

--- js/script.js
+++ js/script.js
@@ -1,140 +1,117 @@
 const select = document.getElementById('select-all'),
-  pages = document.getElementsByClassName('page');
+    pages = document.getElementsByClassName('page');
 
-console.log(select);
 
 select.addEventListener("click", function () {
-  var checkboxes = pages;
-  for (var checkbox of checkboxes) {
-    checkbox.checked = this.checked;
-  }
+    var checkboxes = pages;
+    for (var checkbox of checkboxes) {
+        checkbox.checked = this.checked;
+    }
 });
 
 jQuery(document).ready(function () {
 
-  var updateOutput = function () {
-    $('#nestable-output').val(JSON.stringify($('#nestable').nestable('serialize')));
-  };
+    var updateOutput = function () {
+        $('#nestable-output').val(JSON.stringify($('#nestable').nestable('serialize')));
+    };
 
-  $('#nestable').nestable().on('change', updateOutput);
+    $('#nestable').nestable().on('change', updateOutput);
 
-  updateOutput();
-
-  $("#add-item").submit(function (e) {
-    e.preventDefault();
-    id = Date.now();
-    let checkboxes = pages;
-    for (var checkbox of checkboxes) {
-      if (checkbox.checked) {
-        var checkId = checkbox.id;
-        var label = checkbox.name;
-
-        console.log(checkId);
-
-        var page = $(`input`);
-
-        var id = checkId;
-        var parent_id = page.data("parent-id");
-        var object = page.data("object");
-        var title = label;
-        var type = page.data("type");
-        var url = page.data("url");
-        var target = page.data("target");
-        var attr_title = page.data("attr-title");
-        var current_id = page.data("current-id");
-        var xfn = page.data("xfn");
-
-        $.post(ajaxurl,
-          {
-            'action': 'add-menu-item',
-            'menu-item[-1][menu-item-object-id]': id,
-            'menu-item[-1][menu-item-db-id]': '0',
-            'menu-item[-1][menu-item-object]': object,
-            'menu-item[-1][menu-item-parent-id]': parent_id,
-            'menu-item[-1][menu-item-type]': type,
-            'menu-item[-1][menu-item-title]': title,
-            'menu-item[-1][menu-item-url]': url,
-            'menu-item[-1][menu-item-target]': target,
-            'menu-item[-1][menu-item-attr_title]': attr_title,
-            'menu-item[-1][menu-item-classes]': '',
-            'menu-item[-1][menu-item-xfn]': xfn,
-          },
-          function (response) {
-            if ((label == "")) return;
-            var item =
-              '<li class="dd-item dd3-item" ' +
-              'data-id="' + id + '"' +
-              'data-label="' + title + '"' +
-              'data-object="' + object + '"' +
-              'data-parent-id="' + parent_id + '"' +
-              'data-type="' + type + '"' +
-              'data-url="' + url + '"' +
-              'data-target="' + target + '"' +
-              'data-attr-title="' + attr_title + '"' +
-              'data-current-id="' + current_id + '"' +
-              'data-xfn="' + xfn + '">' +
-              '<div class="dd-handle dd3-handle">Drag</div>' +
-              '<div class="dd3-content">' +
-              '<span>' + label + '</span>' +
-              '<div class="item-edit">Edit</div>' +
-              '</div>' +
-              '<div class="item-settings d-none">' +
-              '<p><label for="">Navigation Label<br><input type="text" name="navigation_label" value="' + label + '"></label></p>' +
-              '<p><a class="item-delete" href="javascript:;">Remove</a> |' +
-              '<a class="item-close" href="javascript:;">Close</a></p>' +
-              '</div>' +
-              '</li>';
-            $("#nestable > .dd-list").append(item);
-            $("#nestable").find('.dd-empty').remove();
-            $("#add-item > [name='name']").val('');
-            $("#add-item > [name='url']").val('');
-            updateOutput();
-            checkbox.checked = false;
-            if (select.checked) {
-              select.checked = false
-            }
-          });
-      } else {
-        console.log("pas check");
-      }
-    }
-  });
-
-  $("body").delegate(".item-delete", "click", function (e) {
-    $(this).closest(".dd-item").remove();
     updateOutput();
-  });
+
+    $("#add-item").submit(function (e) {
+        e.preventDefault();
+        let checkboxes = pages;
+        for (var checkbox of checkboxes) {
+            if (checkbox.checked) {
+
+                var checkId = checkbox.id;
+                var label = checkbox.name;
+                var url = $(`#${checkId}`).data("url");
+
+                if ((label == "")) return;
+                var item =
+                    '<li class="dd-item dd3-item" data-id="' + checkId + '" data-label="' + label + '" data-url="' + url + '">' +
+                    '<div class="dd-handle dd3-handle" > Drag</div>' +
+                    '<div class="dd3-content"><span>' + label + '</span>' +
+                    '<div class="item-edit">Edit</div>' +
+                    '</div>' +
+                    '<div class="item-settings d-none">' +
+                    '<p><label for="">Navigation Label<br><input type="text" name="navigation_label" value="' + label + '"></label></p>' +
+                    '<p><label for="">Navigation Url<br><input type="text" name="navigation_url" value="' + url + '"></label></p>' +
+                    '<p><a class="item-delete" href="javascript:;">Remove</a> |' +
+                    '<a class="item-close" href="javascript:;">Close</a></p>' +
+                    '</div>' +
+                    '</li>';
+
+                $("#nestable > .dd-list").append(item);
+                $("#nestable").find('.dd-empty').remove();
+                $("#add-item > [name='name']").val('');
+                $("#add-item > [name='url']").val('');
+                updateOutput();
+
+                checkbox.checked = false;
+                if (select.checked) {
+                    select.checked = false
+                }
+            }
+        }
+    });
+
+    $("#add-menu").submit(function (e) {
+        e.preventDefault();
+        // $("#result").remove();
+
+        id = Date.now();
+        var menu = $("#add-menu > [name='menu']").serialize();
+        console.log(menu)
+
+        $.ajax({
+            url: 'http://gestionplugin.test/wp-content/plugins/plugin-wordpress/menu.php',
+            method: 'POST',
+            data: menu,
+            success: function (response) {
+                $('#result').fadeIn().text("Vous avez enregistré le menu").delay(2000).fadeOut();
+            },
+            error: function (response) {
+                console.log('Erreur…');
+            }
+        });
+    });
+
+    $("body").delegate(".item-delete", "click", function (e) {
+        $(this).closest(".dd-item").remove();
+        updateOutput();
+    });
 
 
-  $("body").delegate(".item-edit, .item-close", "click", function (e) {
-    var item_setting = $(this).closest(".dd-item").find(".item-settings");
-    if (item_setting.hasClass("d-none")) {
-      item_setting.removeClass("d-none");
-    } else {
-      item_setting.addClass("d-none");
-    }
-  });
+    $("body").delegate(".item-edit, .item-close", "click", function (e) {
+        var item_setting = $(this).closest(".dd-item").find(".item-settings");
+        if (item_setting.hasClass("d-none")) {
+            item_setting.removeClass("d-none");
+        } else {
+            item_setting.addClass("d-none");
+        }
+    });
 
-  $("body").delegate("input[name='navigation_label']", "change paste keyup", function (e) {
-    $(this).closest(".dd-item").data("label", $(this).val());
-    $(this).closest(".dd-item").find(".dd3-content span").text($(this).val());
-  });
+    $("body").delegate("input[name='navigation_label']", "change paste keyup", function (e) {
+        $(this).closest(".dd-item").data("label", $(this).val());
+        $(this).closest(".dd-item").find(".dd3-content span").text($(this).val());
+    });
+    $(this).closest(".dd-item").remove();
+    $('.dd-list').bind('DOMSubtreeModified', function () {
+        if ($('.dd-list').is(':empty')) {
+            $('.is-empty').addClass('d-none');
+        }
+        else {
+            $('.is-empty').removeClass('d-none');
+        }
+    });
 
-  $('.dd-list').bind('DOMSubtreeModified', function(){
-    if ($('.dd-list').is(':empty')){
-      $('.is-empty').addClass('d-none');
-    }
-    else {
-      $('.is-empty').removeClass('d-none');
-    }
-  });
-
-  $("#sup").submit(function (e) {
-    e.preventDefault();
-    var div = '<div class="dd-empty"></div>';
-    $(".dd-list").empty();
-    $("#nestable").append(div);
-  });
-
-
+    $("#sup").submit(function (e) {
+        e.preventDefault();
+        var div = '<div class="dd-empty"></div>';
+        $(".dd-list").children().closest(".dd-item").remove();
+        $("#nestable").prepend(div);
+    });
 });
--- menuHelper.php
+++ menuHelper.php
@@ -1,115 +1,111 @@
 <?php
+
 /*
 Plugin Name: Menu Helper
-Plugin URI: http://gestionplugin.test/
-Description: This is a test
+Plugin URI: https://menu_helper.com/
+Description: Ceci est mon premier plugin
 Author: Nous
-Version: 1.1.0
-Author URI: http://mon-siteweb.com/
+Version: 1.0
+Author URI: http://bonjour.com/
 */
+
+include 'db.php';
 
 function my_admin_menu()
 {
-  add_theme_page('Menu Helper', 'Menu Helper', 'edit_pages', 'menuHelper.php', 'myplguin_admin_page', 3);
-  wp_enqueue_style('styles_css', plugins_url('css/style.css', __FILE__), array());
-  wp_enqueue_style('stylesJqu_css', plugins_url('css/jquery.nestable.css', __FILE__), array());
+    add_theme_page('Menu Helper', 'Menu Helper', 'edit_pages', 'menuHelper.php', 'myplguin_admin_page', 3);
 }
 
 function myplguin_admin_page()
 {
 ?>
-  <div class="wrap">
-    <h2>Bienvenue dans mon plugin</h2>
-  </div>
-  <div class="row">
-    <form id="add-item">
-      <ul>
-        <?php all_pages() ?>
-      </ul>
-      <input type="checkbox" id="select-all" name="all">
-      <label for="select-all">Selected all</label>
-
-      <button type="submit">Ajouter au menu</button>
-    </form>
-    <div class="dd" id="nestable">
-      <?php
-      page_menu();
-      ?>
-
-      <div class="btn">
-        <form id="sup">
-          <button class="is-empty sup d-none">Vider le menu</button>
-        </form>
-        <form id="enregister">
-          <button class="is-empty sup d-none">Enregister</button>
-        </form>
-      </div>
+    <div class="wrap">
+        <h2>Bienvenue sur notre plugin</h2>
     </div>
-  </div>
+    <div class="row">
+        <form id="add-item">
+            <ul>
+                <?php all_pages() ?>
+            </ul>
+            <input type="checkbox" id="select-all" name="all">
+            <label for="select-all">Selected all</label>
+            <button type="submit">Add Item</button>
+        </form>
+
+        <hr />
+
+        <div class="dd" id="nestable">
+            <?php
+            $html_menu = menuTree();
+            echo (empty($html_menu)) ? '<ol class="dd-list"></li></ol>' : $html_menu;
+            ?>
+
+            <div class="btn">
+                <form id="sup">
+                    <button class="is-empty sup">Vider le menu</button>
+                </form>
+                <form id="add-menu" method="post">
+                    <input type="hidden" id="nestable-output" name="menu">
+                    <button type="submit" class="is-empty sup">Save Menu</button>
+                </form>
+            </div>
+            <div id="result"></div>
+        </div>
+    </div>
+
 <?php
-  wp_enqueue_script('script_js', plugins_url('js/script.js', __FILE__), array());
-  wp_enqueue_script('scriptjq_js', plugins_url('js/jquery-3.4.1.min.js', __FILE__), array("jquery"));
-  wp_enqueue_script('scriptjq2_js', plugins_url('js/jquery.nestable.js', __FILE__), array("jquery"));
-  wp_localize_script( 'manage_menu', 'ajax_url', admin_url( 'admin-ajax.php' ) );
+    wp_enqueue_style('styles_css', plugins_url('css/style.css', __FILE__), array());
+    wp_enqueue_style('stylesJqu_css', plugins_url('css/jquery.nestable.css', __FILE__), array());
+    wp_enqueue_script('script_js', plugins_url('js/script.js', __FILE__), array());
+    wp_enqueue_script('scriptjq_js', plugins_url('js/jquery-3.4.1.min.js', __FILE__), array("jquery"));
+    wp_enqueue_script('scriptjq2_js', plugins_url('js/jquery.nestable.js', __FILE__), array("jquery"));
 }
 
 function all_pages()
 {
-  $pages = get_pages();
-  foreach ($pages as $page) {
-    $li = '<li>
-      <input type="checkbox" class="page"' .
-      'id="' . $page->ID . '" ' .
-      'name="' . $page->post_title . '"' .
-      'data-object="page"' .
-      'data-parent-id="' . $page->post_parent . '"' .
-      'data-type="post_type"' .
-      'data-url="' . get_permalink($page->ID) . '"' .
-      'data-target=""' .
-      'data-attr-title=""' .
-      'data-current-id=""' .
-      'data-xfn="">' .
-      '<label for="' . $page->ID . '">';
-    $li .= $page->post_title;
-    $li .= '</label></li>';
-    echo $li;
-  }
+    $pages = get_pages();
+    foreach ($pages as $page) {
+        $li = '<li>
+                <input type="checkbox" class="page" id="' . $page->ID . '" name="' . $page->post_title . '" data-url="' . get_permalink($page->ID) . '">' .
+            '<label for="' . $page->ID . '">';
+        $li .= $page->post_title;
+        $li .= '</label></li>';
+        echo $li;
+    }
 }
 
-function page_menu()
+function renderMenuItem($id, $label, $url)
 {
-?>
-  <ol class="dd-list">
-    </li>
-  </ol>
-<?php
+    return '<li class="dd-item dd3-item" data-id="' . $id . '" data-label="' . $label . '" data-url="' . $url . '">' .
+        '<div class="dd-handle dd3-handle" > Drag</div>' .
+        '<div class="dd3-content"><span>' . $label . '</span>' .
+        '<div class="item-edit">Edit</div>' .
+        '</div>' .
+        '<div class="item-settings d-none">' .
+        '<p><label for="">Navigation Label<br><input type="text" name="navigation_label" value="' . $label . '"></label></p>' .
+        '<p><label for="">Navigation Url<br><input type="text" name="navigation_url" value="' . $url . '"></label></p>' .
+        '<p><a class="item-delete" href="javascript:;">Remove</a> |' .
+        '<a class="item-close" href="javascript:;">Close</a></p>' .
+        '</div>';
 }
 
-
-// function menuItem($id, $label)
-// {
-// 
-?>
-<!-- <li class="dd-item dd3-item" data-id="<?= $id ?>" data-label="<?= $label ?>">
-//     <div class="dd-handle dd3-handle">
-//       Drag
-//     </div>
-//     <div class="dd3-content">
-//       <span><?= $label ?></span>
-//       <div class="item-edit">Edit</div>
-//     </div>
-//     <div class="item-settings d-none">
-//       <p>
-//         <label for="">Titre de la navigation<br>
-//           <input type="text" name="navigation_label" value="<?= $label ?>">
-//         </label>
-//       </p>
-//       <p>
-//         <a class="item-delete" href="javascript:;">Retirer</a> | <a class="item-close" href="javascript:;">Annuler</a>
-//       </p>
-//     </div> -->
-<?php
-// }
-
+function menuTree($parent_id = 0)
+{
+    global $db;
+    $items = '';
+    $query = $db->query("SELECT * FROM menu WHERE parent_id = ? ORDER BY id_menu ASC", $parent_id);
+    if ($query->numRows() > 0) {
+        $items .= '<ol class="dd-list">';
+        $result = $query->fetchAll();
+        foreach ($result as $row) {
+            $items .= renderMenuItem($row['id_menu'], $row['label_menu'], $row['url_menu']);
+            $items .= menuTree($row['id_menu']);
+            $items .= '</li>';
+        }
+        $items .= '</ol>';
+    }
+    return $items;
+}
 
 add_action('admin_menu', 'my_admin_menu');
+?>
