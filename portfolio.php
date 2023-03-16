7619fc4f6e707c65c8c36b76a7832331e317f2f6
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2023-03-16 11:20:36 +0100

    portfolio début

---
+++ portfolio.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Portfolio
+Plugin URI: http://gestionplugin.test/
+Description: This is a test
+Author: Bienfait Alexandre
+Version: 1.0.0
+Author URI: http://mon-siteweb.com/
+*/
+
+function my_admin_menu()
+{
+  add_menu_page('Portfolio', 'Portfolio', 'edit_pages', 'portfolio.php', 'admin_page_content', 'dashicons-id-alt');
+  wp_enqueue_style('styles_css', plugins_url('css/style.css', __FILE__), array());
+}
+
+function admin_page_content()
+{
+?>
+  <div class="wrap">
+    <h2>Bienvenue sur la page configuration de votre portfolio !</h2>
+  </div>
+<?php
+  wp_enqueue_script('script_js', plugins_url('js/script.js', __FILE__), array());
+}
+
+
+add_action('admin_menu', 'my_admin_menu');
--- script.js
+++
@@ -1,8 +0,0 @@
-const select = document.getElementById('select-all');
-
-select.addEventListener("click", function() {
-    var checkboxes = document.getElementsByClassName('page');
-    for (var checkbox of checkboxes) {
-        checkbox.checked = this.checked;
-    }
-});
